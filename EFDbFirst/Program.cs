0ca599710ece05df2eb56acfba1abf82bf7977ad
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-12-12 19:40:15 +0530

    Added and used enum Level in Enum Types folder in conceptual folder for Level field in courses by pointing to existing enum Level in our project

--- EFDbFirst/Program.cs
+++ EFDbFirst/Program.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace EFDbFirst
 {
+    // Inheriting from byte since it is easy mapping between tiny int in database to byte in entity
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+
+    }
     class Program
     {
+       
         static void Main(string[] args)
         {
             var dbContext = new PlutoDbContext();
